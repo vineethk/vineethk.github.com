8c1fe3d6ca64bce9b4cf1239df1b66f87cc7ddea
Vineeth Kashyap <vineeth@cs.ucsb.edu>
2011-10-06 12:12:37 -0700

    Adding a new index page, trying out stuff

---
+++ navigation.js
@@ -0,0 +1,25 @@
+
+<div id="wrapper">
+<div id="header"> 
+<div id="menu">
+<ul>
+<li><a href="index.html">Home</a></li>
+<li><a href="http://phdstuff.wordpress.com/">Blog</a></li>
+<li><a href="photos.html">Photos</a></li>
+<li><a href="research.html">Research</a></li>
+<li> <script>document.write('<a href="mailto:' + 'vineet' + ["h", "k"][0] + '@' + 'cs.ucsb.edu"> Contact</a>'); </script></li>
+</ul>
+</div>
+
+<div id="logo">
+<h1><a href="#">Vineeth Kashyap</a><br>
+</h1>
+<p>Graduate Student Researcher, UCSB
+</p>
+</div>
+<hr>
+
+<div id="page">
+<div id="content">
+
+
